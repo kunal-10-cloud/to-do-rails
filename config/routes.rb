ec86ac1004c79c6d1ff4fa4049154e789f61839e
Aditya Bijalwan <abijalwan586@gmail.com>
2025-06-11 14:13:45 +0530

    fix: lint errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
   root "tasks#index"
-  resources :tasks, only: [:index, :create, :update, :destroy]
-  
+  resources :tasks, only: [ :index, :create, :update, :destroy ]
 end
